1369c6e48dc7bfd5b6db41da2571f6314f959487
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-09-09 09:00:17 +0300

    Update the productview to avoid duplication of the code and use a array then loop througth it

---
+++ src/assets/js/productdata.js
@@ -0,0 +1,219 @@
+// the Products listed here for better accessiblity
+export const products = [
+    {
+        name: 'Crocodile',
+        price: 1400,
+        color: 'Brown',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image1.jpeg',
+    },
+
+    {
+        name: 'Air Max',
+        price: 1300,
+        color: 'Pink',
+        status: 'Available',
+        size: 'No.36',
+        image: '/src/assets/images/image2.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1400,
+        color: 'White & Black',
+        status: 'Available',
+        size: 'No.36',
+        image: '/src/assets/images/image3.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1400,
+        color: 'White',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image5.jpeg',
+    },
+
+    {
+        name: 'Nike SB',
+        price: 1400,
+        color: 'Pink & Pruple',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image6.jpeg',
+    },
+
+    {
+        name: 'J1 Low Cut',
+        price: 1400,
+        color: 'Red & White',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image7.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1800,
+        color: 'Black',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image9.jpeg',
+    },
+
+    {
+        name: 'J1 Low Cut',
+        price: 1500,
+        color: 'Brown',
+        status: 'Available',
+        size: 'No.36',
+        image: '/src/assets/images/image10.jpeg',
+    },
+
+    {
+        name: 'Nike SB',
+        price: 1400,
+        color: 'White & Black',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image11.jpeg',
+    },
+
+    {
+        name: 'J1 low cut',
+        price: 1050,
+        color: 'White & Red',
+        status: 'Available',
+        size: 'No.36',
+        image: '/src/assets/images/image12.jpeg',
+    },
+
+    {
+        name: 'Boot',
+        price: 1400,
+        color: 'Brown & Black',
+        status: 'Available',
+        size: 'No.38',
+        image: '/src/assets/images/image14.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1400,
+        color: 'grey & Pink',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image15.jpeg',
+    },
+
+    {
+        name: 'J11',
+        price: 1700,
+        color: 'White',
+        status: 'Available',
+        size: 'No.43',
+        image: '/src/assets/images/image16.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1400,
+        color: 'Orange',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image17.jpeg',
+    },
+
+    {
+        name: 'Adidas',
+        price: 1400,
+        color: 'White',
+        status: 'Available',
+        size: 'No.38',
+        image: '/src/assets/images/image18.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1400,
+        color: 'White & Green',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image19.jpeg',
+    },
+
+    {
+        name: 'J1 Low cut',
+        price: 1500,
+        color: 'White & Black',
+        status: 'Available',
+        size: 'No.38',
+        image: '/src/assets/images/image21.jpeg',
+    },
+
+    {
+        name: 'J1 Low cut',
+        price: 1400,
+        color: 'White & Red',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image22.jpeg',
+    },
+
+    {
+        name: 'J1 Low cut',
+        price: 1500,
+        color: 'grey & ligth Blue',
+        status: 'Available',
+        size: 'No.38',
+        image: '/src/assets/images/image23.jpeg',
+    },
+    
+    {
+        name: 'J3',
+        price: 1500,
+        color: 'Blue',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image24.jpeg',
+    },
+
+    {
+        name: 'Puma',
+        price: 1500,
+        color: 'ligth Brown',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image25.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1500,
+        color: 'White',
+        status: 'Available',
+        size: 'No.38',
+        image: '/src/assets/images/image26.jpeg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1400,
+        color: 'pink & White',
+        status: 'Available',
+        size: 'No.38',
+        image: '/src/assets/images/image27.jpeg',
+    },
+
+    {
+        name: 'J3',
+        price: 1500,
+        color: 'Blue',
+        status: 'Available',
+        size: 'No.40',
+        image: '/src/assets/images/image28.jpeg',
+    },
+];
+  
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,397 +1,43 @@
 <script setup>
-import Navbar from '@/components/navbarComponent.vue'
-import Footer from '@/components/footerComponent.vue'
+import Navbar from '@/components/navbarComponent.vue';
+import Footer from '@/components/footerComponent.vue';
+import { products } from '@/assets/js/productdata.js';
 </script>
+
+
+
 
 <template>
     <Navbar/>
 
-    <div class="product_container main_container content_container ">
-        <div class="collection">   
-            <h2>Our Shoes</h2>
-                     
+    <div class="product_container main_container content_container">
+        <div class="collection">
+        <h2>Our Collection</h2>
+        <p>We have <strong>{{ products.length }}</strong> Shoes in our Collection</p>
+
             <div class="collection_items">
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image1.jpeg" alt="shoe Image">
+                <div v-for="(product, index) in products" :key="index">
+                    <div class="collection_infor">
+                        <div class="img">
+                            <img :src="product.image" :alt="`Image of ${product.name}`" />
+                        </div>
+                        <div class="group">
+                            <p>Name: <span>{{ product.name }}</span></p>
+                            <p class="price">Ksh. {{ product.price }}</p>
+                        </div>
+                        <p>Color: <strong>{{ product.color }}</strong></p>
+                        <div class="group">
+                            <p class="status">Available</p>
+                            <p>Size: <strong>{{ product.size }}</strong></p>
+                        </div>
+                        <button>
+                            <a :href="'https://wa.me/254769696309?text=' + encodeURIComponent('Lawbert shoes, inquiry about ' + product.name)" target="_blank">Inquire Now</a>
+                        </button>
                     </div>
-                    <div class="group">
-                        <p>Name: <span>crocodile</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>Brown</strong></p>
-                    <div class="group">
-                        <p class="status">Availbale</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image2.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Air Max</span></p>
-                        <p class="price">Ksh. 1,300</p>
-                    </div>
-                    <p>Color: <strong>Pink</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.36</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image3.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J1</span></p>
-                        <p class="price">Ksh. 1,500</p>
-                    </div>
-                    <p>Color: <strong>Black & White</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image15.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Air Force</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>Grey & pink</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.38</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image5.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Air Force</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>White</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image6.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Nike SB</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>Purple & blue</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.38</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image7.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J1 low cut</span></p>
-                        <p class="price">Ksh. 1,300</p>
-                    </div>
-                    <p>Color: <strong>White & Red</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.38</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image9.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Air Force</span></p>
-                        <p class="price">Ksh. 1,800</p>
-                    </div>
-                    <p>Color: <strong>Black</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.42</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image10.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J4</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>light Brown</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.36</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image11.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Nike SB</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>White & Black</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.41</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image12.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J1 low cut</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>White & Red</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.36</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <!-- new -->
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image14.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Boot</span></p>
-                        <p class="price">Ksh. 1,300</p>
-                    </div>
-                    <p>Color: <strong>Brown</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.38</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image16.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span> J11 </span></p>
-                        <p class="price">Ksh. 1,800</p>
-                    </div>
-                    <p>Color: <strong>White & Blue</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.43</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image17.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Air Force</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>Orange</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image18.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Adidas</span></p>
-                        <p class="price">Ksh. 1,300</p>
-                    </div>
-                    <p>Color: <strong>White</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.38</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image19.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Air Force</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>White & Green</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image20.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>Air Force </span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>White</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image21.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J1 low cut</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>White & Black</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.36</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image22.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J1 low cut</span></p>
-                        <p class="price">Ksh. 1,300</p>
-                    </div>
-                    <p>Color: <strong>White & Red</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image23.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J1 low cut</span></p>
-                        <p class="price">Ksh. 1,300</p>
-                    </div>
-                    <p>Color: <strong>Green & White</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.38</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
-                </div>
-
-                <div class="collection_infor">
-                    <div class="img">
-                        <img src="/src/assets/images/image24.jpeg" alt="shoe Image">
-                    </div>
-                    <div class="group">
-                        <p>Name: <span>J3</span></p>
-                        <p class="price">Ksh. 1,400</p>
-                    </div>
-                    <p>Color: <strong>Blue & White</strong></p>
-                    <div class="group">
-                        <p class="status">Available</p>
-                        <p>Size: <strong>No.40</strong></p>
-                    </div>
-                    <button>
-                        <a href="https://wa.me/254769696309?text='Lawbert shoes'" target="_blank">Inquire Now</a>
-                    </button>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <Footer/>
 </template>
