3f57d81df2d84df2c052d2a6e0342527285fc098
lawrencemwangingige@gmail.com <lawrencemwangingige@gmail.com>
2024-09-20 12:00:10 +0300

    Added some pic for the arrives and update the page to be displaying the new product arrives

---
+++ src/assets/js/arrivesdata.js
@@ -0,0 +1,38 @@
+export const products = [
+
+    {
+        name: 'Nike Zoom',
+        price: 1400,
+        color: 'Brown & Black',
+        status: 'Available',
+        size: 'No.37',
+        image: '/assets/images/images3.jpg',
+    },
+
+    {
+        name: 'New Balance',
+        price: 1300,
+        color: 'White & grey',
+        status: 'Available',
+        size: 'No.38',
+        image: '/assets/images/images1.jpg',
+    },
+
+    {
+        name: 'Air Force',
+        price: 1400,
+        color: 'White',
+        status: 'Available',
+        size: 'No.41',
+        image: '/assets/images/images2.jpg',
+    },
+
+    {
+        name: 'New Balance',
+        price: 1400,
+        color: 'White & Grey',
+        status: 'Available',
+        size: 'No.40',
+        image: '/assets/images/images4.jpg',
+    },
+];
--- src/views/ArrivesView.vue
+++ src/views/ArrivesView.vue
@@ -1,17 +1,41 @@
 <script setup>
 import Navbar from '@/components/navbarComponent.vue'
 import Footer from '@/components/footerComponent.vue'
+import { products } from '@/assets/js/arrivesdata.js';
 </script>
 
 
 <template>
    <Navbar/>
 
-   <div class="arrives_container">
+    <div class="arrives_container main_container content_container">
       <h1>Our New Arrives</h1>
 
-      <h3>NO new Arrives at the moment</h3>
-   </div>
+        <div class="collection">
+            <p>Here are our new arrives for <strong>(20/09/2024)</strong></p>
+            <div class="collection_items">
+                <div v-for="(product, index) in products" :key="index">
+                    <div class="collection_infor">
+                        <div class="img">
+                                <img :src="product.image" :alt="`Image of ${product.name}`" />
+                        </div>
+                        <div class="group">
+                                <p>Name: <span>{{ product.name }}</span></p>
+                                <p class="price">Ksh. {{ product.price }}</p>
+                        </div>
+                        <p>Color: <strong>{{ product.color }}</strong></p>
+                        <div class="group">
+                                <p class="status">Available</p>
+                                <p>Size: <strong>{{ product.size }}</strong></p>
+                        </div>
+                        <button>
+                                <a :href="'https://wa.me/254769696309?text=' + encodeURIComponent('Lawbert shoes, inquiry about ' + product.name)" target="_blank">Inquire Now</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
    
    <Footer/>
 </template>
